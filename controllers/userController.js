c087b1b34f3be121d0d04149bbc0cb3c5ab26cfc
NwankwoMichael <mikaelicious99@gmail.com>
2026-06-03 00:24:24 -0700

    Fix: add robust image fallback handling to header template

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,7 +45,10 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(path.join(__dirname, `../public/img/users`, req.file.filename));
+    // .toFile(path.join(__dirname, `../public/img/users`, req.file.filename));
+    .toFile(
+      path.join(process.cwd(), 'public', 'img', 'users', req.file.filename),
+    );
 
   next();
 });
